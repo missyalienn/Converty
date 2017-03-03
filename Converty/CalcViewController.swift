4bcf0723b378c550f15a106ce0e5d6b7ba39f839
missyalienn <allan.missy@gmail.com>
2017-03-03 17:51:17 -0500

    min calculator functionality working

---
+++ Converty/CalcViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Converty
+//
+//  Created by Missy Allan on 3/2/17.
+//  Copyright © 2017 PandaLabs. All rights reserved.
+//
+
+import UIKit
+
+class CalcViewController: UIViewController {
+
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userInMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle! 
+        print("touched \(digit) digit")
+        
+        if userInMiddleOfTyping == true {
+            let textCurrentlyInDisplay = display!.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+        }
+        userInMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+          return Double(display.text!)!
+        }set {
+            display.text = String(newValue)
+        }
+    }
+    
+    
+    private var brain = CalcBrain()
+    
+    
+    @IBAction private func performOperation(_ sender: UIButton) {
+        
+        if userInMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userInMiddleOfTyping = false
+        }
+        
+        if let mathSymbol = sender.currentTitle {
+           brain.performOperation(symbol: mathSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

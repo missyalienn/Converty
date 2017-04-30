c89d6569dc00896947b37c0994de0643b84faabb
missyalienn <allan.missy@gmail.com>
2017-04-30 12:45:19 -0400

    identifiers added

---
+++ Identifier.swift
@@ -0,0 +1,29 @@
+//
+//  Identifier.swift
+//  Converty
+//
+//  Created by Missy Allan on 4/30/17.
+//  Copyright © 2017 PandaLabs. All rights reserved.
+//
+
+import Foundation
+
+struct Identifier {
+    
+    static let stopID = "alarmStop"
+    static let snoozeID = "alarmSnooze"
+    static let addSegueID = "addSegue"
+    static let editSegueID = "editSegue"
+    static let saveSegueID = "saveEditSegue"
+    static let soundSegueID = "soundSegue"
+    static let labelSegueID = "labelEditSegue"
+    static let weekdaysSegueID = "weekdaysSegue"
+    static let settingID = "setting"
+    static let musicID = "musicIdentifier"
+    static let alarmCellID = "alarmCell"
+    
+    static let labelUnwindID = "labelUnwindSegue"
+    static let soundUnwindID = "soundUnwindSegue"
+    static let weekdaysUnwindID = "weekdaysUnwindSegue"
+    
+}
